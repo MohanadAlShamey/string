3f64b88628e22c94f3111c80958d5b26be2252ca
Mh_Shamey <mh.shamey@gmail.com>
2022-11-20 09:50:37 +0300

    lesson 2

---
+++ lib/remember.dart
@@ -0,0 +1,15 @@
+String x="test"
+    "test2"
+    "test3";
+String y='''
+fsfdssdf
+fsdfsdfds
+sdfdsfs
+
+''';
+//print(x.replaceAll('te', 're'));
+//print (x.split(' '));
+// String x='heelo wer';
+// List<String> y=['hello','world'];
+// x=y.join('#');
+// print(x);
